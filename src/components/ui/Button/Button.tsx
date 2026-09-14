db777e8b7594fb9bc350d5e52417523063306072
champix56 <desorbaix@free.fr>
2026-09-14 14:55:38 +0200

    multi class en css

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import styles from './Button.module.css'
 const Button:React.FunctionComponent=()=>{
-    return <button className={styles.Button}>Benjamin</button>
+    return <button className={`${styles.Button} ${styles.primary}`}>Benjamin</button>
 }
 export default Button
