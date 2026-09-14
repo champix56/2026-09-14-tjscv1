080c3810e4396003ef50dc0fb97f81515d6855a8
champix56 <desorbaix@free.fr>
2026-09-14 14:49:04 +0200

    style module

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
-import './Button.css'
+import styles from './Button.module.css'
 const Button:React.FunctionComponent=()=>{
-    return <button className="Button">Benjamin</button>
+    return <button className={styles.Button}>Benjamin</button>
 }
 export default Button
