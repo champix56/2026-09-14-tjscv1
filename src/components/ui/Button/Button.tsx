369da78c77582a3722a3723a1c85c06d0e316b14
champix56 <desorbaix@free.fr>
2026-09-14 16:40:35 +0200

    gestion dynamique de classe par fonction (peut etre fait par ternaire)

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,16 +1,26 @@
-import type React from "react";
+import React from "react";
 import styles from "./Button.module.css";
 /*type T_Buttonprops = {
   type: string;
   text: any;
 };*/
 interface I_ButtonProps {
-  type?: "button"|"submit"|"reset";
-  children:React.ReactNode|Array<React.ReactNode>
+  type?: "button" | "submit" | "reset";
+  children: React.ReactNode | Array<React.ReactNode>;
+  className?: "primary" | "danger";
 }
-const Button: React.FC<I_ButtonProps> = ({children,type}) => {
+const Button: React.FC<I_ButtonProps> = ({ children, type, className }) => {
+  const getClassNameFromProps = () => {
+    if (className) return " " + styles[className];
+    else return "";
+  };
   return (
-    <button className={`${styles.Button} ${styles.primary}`} type={type}>{children}</button>
+    <button
+      className={`${styles.Button}${getClassNameFromProps()}`}
+      type={type}
+    >
+      {children}
+    </button>
   );
 };
 export default Button;
