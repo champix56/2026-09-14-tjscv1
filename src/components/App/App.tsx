f366f9c3c0a02785a10e3a5f1a7a3241ecfa7e7a
champix56 <desorbaix@free.fr>
2026-09-15 10:05:44 +0200

    base fonction pour useState

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button, { PrimaryButton } from "../ui/Button/Button";
 import "./App.css";
-/*
+
 function App() {
   let counter = 0;
   return (
@@ -27,42 +27,6 @@ function App() {
       </Button>
     </div>
   );
-}*/
-class App extends React.Component<any,{counter:number;autreValue:string}>{
-
-  constructor(props:any){
-    super(props)
-    this.state={counter:0, autreValue:'abc'}
-  }
-
-  componentDidMount(): void {}
-  componentWillUnmount(): void {}
-  componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<{ counter: number; autreValue: string; }>, snapshot?: any): void {
-    console.log('did Update', this.state);
-  }
-  render(){
-    return <div className="App">
-      valeur du counter : {this.state.counter}
-      <hr />
-      <PrimaryButton
-        onButtonClick={() => {
-          this.setState({counter:this.state.counter-1});
-          console.log(this.state.counter);
-        }}
-      >
-        -1
-      </PrimaryButton>
-      <Button
-        className="danger"
-        onButtonClick={() => {
-                    this.setState({counter:this.state.counter+1});
-
-          console.log(this.state.counter);
-        }}
-      >
-        +1
-      </Button>
-    </div>
-  }
 }
+
 export default App;
