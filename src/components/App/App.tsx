a9bddfa8db305d44ff06dbf6c8f4a8f83786670c
champix56 <desorbaix@free.fr>
2026-09-15 09:55:42 +0200

    demo class

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,68 @@
-import Button from "../ui/Button/Button";
+import React from "react";
+import Button, { PrimaryButton } from "../ui/Button/Button";
 import "./App.css";
-
+/*
 function App() {
+  let counter = 0;
   return (
     <div className="App">
-      DEMAT BREIZH
-      <Button style={{border:'1px solid black',textDecoration:'underline' }} onButtonClick={()=>{
-        console.log('coucou')
-      }}  >
-        Text
-      </Button>
-      <Button className="primary"  onButtonClick={()=>{
-        console.log('toto')
-      }} >
-        <div>div</div>
+      valeur du counter : {counter}
+      <hr />
+      <PrimaryButton
+        onButtonClick={() => {
+          counter--;
+          console.log(counter);
+        }}
+      >
+        -1
+      </PrimaryButton>
+      <Button
+        className="danger"
+        onButtonClick={() => {
+          counter++;
+          console.log(counter);
+        }}
+      >
+        +1
       </Button>
     </div>
   );
-}
+}*/
+class App extends React.Component<any,{counter:number;autreValue:string}>{
 
+  constructor(props:any){
+    super(props)
+    this.state={counter:0, autreValue:'abc'}
+  }
+
+  componentDidMount(): void {}
+  componentWillUnmount(): void {}
+  componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<{ counter: number; autreValue: string; }>, snapshot?: any): void {
+    console.log('did Update', this.state);
+  }
+  render(){
+    return <div className="App">
+      valeur du counter : {this.state.counter}
+      <hr />
+      <PrimaryButton
+        onButtonClick={() => {
+          this.setState({counter:this.state.counter-1});
+          console.log(this.state.counter);
+        }}
+      >
+        -1
+      </PrimaryButton>
+      <Button
+        className="danger"
+        onButtonClick={() => {
+                    this.setState({counter:this.state.counter+1});
+
+          console.log(this.state.counter);
+        }}
+      >
+        +1
+      </Button>
+    </div>
+  }
+}
 export default App;
