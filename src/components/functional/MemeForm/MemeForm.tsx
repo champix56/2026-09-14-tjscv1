8b05ec96c028a0d8aac68519a156420fa18f0655
champix56 <desorbaix@free.fr>
2026-09-15 17:06:14 +0200

    <label for    -> <label htmlFor

--- src/components/functional/MemeForm/MemeForm.tsx
+++ src/components/functional/MemeForm/MemeForm.tsx
@@ -16,13 +16,13 @@ const MemeForm: FC<IMemeFormProps> = ({ }) => {
   return (
     <div className={styles.MemeForm} data-testid="MemeForm">
             <form>
-        <label for="titre">
+        <label htmlFor="titre">
           <h1>Titre</h1>
         </label>
         <br />
         <input name="titre" id="titre" value="React is easy" />
         <hr />
-        <label for="image">
+        <label htmlFor="image">
           <h2>Image</h2>
         </label>
         <br />
@@ -33,13 +33,13 @@ const MemeForm: FC<IMemeFormProps> = ({ }) => {
           <option value="4">gwenadu.jpg</option>
         </select>
         <hr />
-        <label for="text">
+        <label htmlFor="text">
           <h2>texte</h2>
         </label>
         <br />
         <input name="text" id="text" type="text" value="Le js m'a tué" />
         <br />
-        <label for="x">
+        <label htmlFor="x">
           <h2 style={{ display: "inline" }}>x :</h2>
         </label>
         <input
@@ -49,7 +49,7 @@ const MemeForm: FC<IMemeFormProps> = ({ }) => {
           type="number"
           value="121"
         />
-        <label for="y">
+        <label htmlFor="y">
           <h2 style={{ display: "inline" }}>y :</h2>
         </label>
         <input
@@ -62,12 +62,12 @@ const MemeForm: FC<IMemeFormProps> = ({ }) => {
         <hr />
         <br />
         <h2>Decorations</h2>
-        <label for="color">
+        <label htmlFor="color">
           <h2 style={{ display: "inline" }}>color :</h2>
         </label>
         <input name="color" id="color" type="color" value="#FFFFFF" />
         <br />
-        <label for="fontSize">
+        <label htmlFor="fontSize">
           <h2 style={{ display: "inline" }}>font-size :</h2>
         </label>
         <input
@@ -80,7 +80,7 @@ const MemeForm: FC<IMemeFormProps> = ({ }) => {
         />
         px
         <br />
-        <label for="fontWeight">
+        <label htmlFor="fontWeight">
           <h2 style={{ display: "inline" }}>font-weight :</h2>
         </label>
         <input
@@ -96,19 +96,19 @@ const MemeForm: FC<IMemeFormProps> = ({ }) => {
         <br />
         <input name="underline" id="underline" type="checkbox" />
         &nbsp;
-        <label for="underline">
+        <label htmlFor="underline">
           <h2 style={{ display: "inline" }}>underline</h2>
         </label>
         &nbsp;<h2 style={{display: 'inline'}}>/</h2>
         &nbsp;
-        <label for="italic">
+        <label htmlFor="italic">
           <h2 style={{ display: "inline" }}>italic</h2>
         </label>
         &nbsp;
         <input name="italic" id="italic" type="checkbox" />
         <hr />
         <br />
-        <label for="frameSizeX">
+        <label htmlFor="frameSizeX">
           <h2 style={{ display: "inline" }}>frame size X :</h2>
         </label>
         <input
@@ -120,7 +120,7 @@ const MemeForm: FC<IMemeFormProps> = ({ }) => {
           value="0"
         />
         px{" "}
-        <label for="frameSizeY">
+        <label htmlFor="frameSizeY">
           <h2 style={{ display: "inline" }}>frame size y :</h2>
         </label>
         <input
