59cd8e41cde254de60585d58697a276459ca296e
champix56 <desorbaix@free.fr>
2026-09-15 15:42:24 +0200

    structure principale de l'app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,53 +1,25 @@
-import { useEffect, useState } from "react";
-import Button, { PrimaryButton } from "../ui/Button/Button";
-import "./App.css";
+import MemeForm from "../functional/MemeForm/MemeForm";
+import FlexH1RstGrow from "../layouts/FlexH1rstGrow/FlexH1rstGrow";
+import FlexV3rdGRow from "../layouts/FlexV3rdGRow/FlexV3rdGRow";
+import Footer from "../ui/Footer/Footer";
+import Header from "../ui/Header/Header";
+import MemeSvgViewer from "../ui/MemeSvgViewer/MemeSvgViewer";
+import Navbar from "../ui/Navbar/Navbar";
+//import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  //const [table, setTable] = useState<Array<any>>([])
-  //montage uniquement
-    useEffect(() => {
-    console.log('mount uniquement');
-    setCounter(-1000)
-    return () => {
-      //demontage
-    }
-  }, [])
-  //mount/update sur variable observé
-  useEffect(() => {
-    console.log('mount/update', counter)
-    return () => {
-      //demontage
-    }
-  }, [counter])
-  //ts les refresh
-  useEffect(() => {
-    console.log('ts update')
-  })
-
-
 
   return (
     <div className="App">
-      valeur du counter : {counter}
-      <hr />
-      <PrimaryButton
-        onButtonClick={() => {
-          setCounter(counter-1);
-          console.log(counter);
-        }}
-      >
-        -1
-      </PrimaryButton>
-      <Button
-        className="danger"
-        onButtonClick={() => {
-          setCounter(counter+1);
-          console.log(counter);
-        }}
-      >
-        +1
-      </Button>
+     <FlexV3rdGRow>
+        <Header/>
+        <Navbar/>
+        <FlexH1RstGrow>
+          <MemeSvgViewer/>
+          <MemeForm/>
+        </FlexH1RstGrow>
+        <Footer/>
+      </FlexV3rdGRow>
     </div>
   );
 }
