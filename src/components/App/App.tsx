b3bbfb3fed03d3844a4e39d1aebbc67e3ce5dab5
champix56 <desorbaix@free.fr>
2026-09-15 11:31:33 +0200

    useEffect

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import Button, { PrimaryButton } from "../ui/Button/Button";
 import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0)
   //const [table, setTable] = useState<Array<any>>([])
+  //montage uniquement
+    useEffect(() => {
+    console.log('mount uniquement');
+    setCounter(-1000)
+    return () => {
+      //demontage
+    }
+  }, [])
+  //mount/update sur variable observé
+  useEffect(() => {
+    console.log('mount/update', counter)
+    return () => {
+      //demontage
+    }
+  }, [counter])
+  //ts les refresh
+  useEffect(() => {
+    console.log('ts update')
+  })
+
+
+
   return (
     <div className="App">
       valeur du counter : {counter}
