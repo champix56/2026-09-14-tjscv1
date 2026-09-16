8f66655b2d3f546501c1d5cd01d7d8698e829f80
champix56 <desorbaix@free.fr>
2026-09-16 11:11:01 +0200

    fetch datas

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { emptyMeme, MemeSVGViewer } from "orsys-tjs-meme";
+import { emptyMeme, MemeSVGViewer, type ImageInterface } from "orsys-tjs-meme";
 import MemeForm from "../functional/MemeForm/MemeForm";
 import FlexH1RstGrow from "../layouts/FlexH1rstGrow/FlexH1rstGrow";
 import FlexV3rdGRow from "../layouts/FlexV3rdGRow/FlexV3rdGRow";
@@ -6,10 +6,14 @@ import Footer from "../ui/Footer/Footer";
 import Header from "../ui/Header/Header";
 import Navbar from "../ui/Navbar/Navbar";
 //import "./App.css";
-import { images } from "../../../db.json";
-import { useState } from "react";
+//import { images } from "../../../db.json";
+import { useEffect, useState } from "react";
 function App() {
   const [current, setCurrent] = useState(emptyMeme);
+  const [images, setImages] = useState<Array<ImageInterface>>([])
+  useEffect(() => {
+    fetch('http://localhost:5679/images').then(r=>r.json()).then(array=>setImages(array))
+  }, [])
   return (
     <div className="App">
       <FlexV3rdGRow>
