6855caf93560c8e9da3b81856f293d55317485b7
champix56 <desorbaix@free.fr>
2026-09-16 16:18:26 +0200

    add ressourcesReducer + initial loading (by async thunk)

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
-import currentReducer, { updateCurrent } from "./currentSlice";
+import currentReducer from "./currentSlice";
 import ressourcesReducer from "./ressourcesSlice";
+import { loadAllRessources } from "./asyncThunk";
+
 
 export const store=configureStore({
 
-    reducer:{current:currentReducer}
+    reducer:{current:currentReducer, ressources:ressourcesReducer}
 })
 
 store.subscribe(()=>{
     console.log(store.getState());
 })
-
-/*store.dispatch(updateCurrent({...store.getState().current.meme,text:'zertyu'}))
-store.dispatch(updateCurrent({...store.getState().current.meme,titre:'zertyu'}))
+export type RootState = ReturnType<typeof store.getState>;
+//setInterval(()=>{},10000)
+store.dispatch(loadAllRessources())
+/*store.dispatch(updateCurrent({...store.getState().current.meme,titre:'zertyu'}))
 store.dispatch(updateCurrent({...store.getState().current.meme,x:123}))*/
+//fetch('http://localhost:5679/images').then(r=>r.json()).then(a=>{store.dispatch(addImageList(a))})
