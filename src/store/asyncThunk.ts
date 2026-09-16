81d03704fcee27563b1168dcec1c1bae37f479dc
champix56 <desorbaix@free.fr>
2026-09-16 16:17:54 +0200

    gestion des asynthunk

---
+++ src/store/asyncThunk.ts
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import type { ImageInterface, MemeInterface } from "orsys-tjs-meme";
+
+export const loadAllRessources = createAsyncThunk("current/load", async () => {
+  const pri = fetch("http://localhost:5679/images");
+  const prm = fetch("http://localhost:5679/memes");
+  const prall = await Promise.all([pri, prm]);
+  return { images: await prall[0].json(), memes: await prall[1].json() } as {
+    images: Array<ImageInterface>;
+    memes: Array<MemeInterface>;
+  };
+});
+/*
+export const loadUniqueImage = createAsyncThunk("current/loadUnique", async (id) => {
+  const pri = await fetch("http://localhost:5679/images/"+id);
+  return { image: await pri.json() } as {
+    image: ImageInterface;};
+});
+
+store.dispatch(loadUniqueImage(5))
+
+builder.addCase(loadUniqueImage.fulfilled,(s,a)=>{
+  s.images.push(a.payload.image)
+})
+*/
