0d558c57ce7722a81961644339cd9ff4a64d6aca
champix56 <desorbaix@free.fr>
2026-09-16 16:57:38 +0200

    React.reactFragment <></>

---
+++ src/pages/Editor.tsx
@@ -0,0 +1,13 @@
+import MemeSvgViewer from '../components/ui/MemeSvgViewer/MemeSvgViewer'
+import MemeForm from '../components/functional/MemeForm/MemeForm.stored'
+
+const Editor = () => {
+  return (
+    <>
+        <MemeSvgViewer />
+        <MemeForm />
+    </>
+  )
+}
+
+export default Editor
