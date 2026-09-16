143c4805bb57900180a6e3a2e7a6f3d8519107a6
champix56 <desorbaix@free.fr>
2026-09-16 17:41:49 +0200

    manage route param on change param id value

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,7 +1,35 @@
 import MemeSvgViewer from '../components/ui/MemeSvgViewer/MemeSvgViewer'
 import MemeForm from '../components/functional/MemeForm/MemeForm.stored'
+import { useLocation, useNavigate, useParams } from 'react-router'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import type { AppDispatch, RootState } from '../store/store'
+import { clearCurrent, updateCurrent } from '../store/currentSlice'
 
 const Editor = () => {
+  const ressources=useSelector((s:RootState)=>s.ressources)
+  const params=useParams()
+  const loc=useLocation()
+  const nav=useNavigate()
+  const dispatch = useDispatch<AppDispatch>()
+  console.log(params)
+  console.log(loc)
+  useEffect(() => {
+    if(!ressources.isLoaded){return;}
+    if(params.id===undefined){
+      dispatch(clearCurrent())
+    }
+    else{
+      let foundMeme=ressources.memes.find(m=>m.id===Number(params.id))
+      if(!foundMeme){
+        nav('/')
+      }
+      else{
+        dispatch(updateCurrent(foundMeme))
+      }
+    }
+  }, [params,ressources,dispatch,nav])
+
   return (
     <>
         <MemeSvgViewer />
