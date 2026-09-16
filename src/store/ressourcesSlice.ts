8b74ea157e5609d280f12afed3a64a332f4e6945
champix56 <desorbaix@free.fr>
2026-09-16 16:39:51 +0200

    reformat

--- src/store/ressourcesSlice.ts
+++ src/store/ressourcesSlice.ts
@@ -1,48 +1,51 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { ImageInterface, MemeInterface } from 'orsys-tjs-meme';
-import { loadAllRessources, saveCurrent } from './asyncThunk';
-import { updateCurrent } from './currentSlice';
+import { createSlice } from "@reduxjs/toolkit";
+import type { ImageInterface, MemeInterface } from "orsys-tjs-meme";
+import { loadAllRessources, saveCurrent } from "./asyncThunk";
+import { updateCurrent } from "./currentSlice";
 
-interface IRessourcesState{
-    images:Array<ImageInterface>;
-    memes:Array<MemeInterface>
+interface IRessourcesState {
+  images: Array<ImageInterface>;
+  memes: Array<MemeInterface>;
 }
 
-const initialState:IRessourcesState = {
-    images:[],
-    memes:[]
-}
+const initialState: IRessourcesState = {
+  images: [],
+  memes: [],
+};
 
 const ressourcesSlice = createSlice({
-  name: 'ressources',
+  name: "ressources",
   initialState,
-  reducers: {
-
-  },
+  reducers: {},
   extraReducers(builder) {
-      builder.addCase(loadAllRessources.fulfilled,(state,action)=>{
-        /*state.images=action.payload.images
+    builder.addCase(loadAllRessources.fulfilled, (state, action) => {
+      /*state.images=action.payload.images
         state.memes=action.payload.memes*/
-        return action.payload
-
-      })
-       builder.addCase(saveCurrent.fulfilled,(state,action)=>{
-              const position=state.memes.findIndex(m=>m.id===action.payload.id)
-              if(position===-1){state.memes.push(action.payload)}
-              else
-              {
-                state.memes[position]=action.payload
-              }
-          })
-      /*demo ecoute daction d'autre tranches*/
-      builder.addCase(updateCurrent,(s,a:{type:string,payload:MemeInterface})=>{
-        console.log('depuis ressources j\'ecoute laction de update current',s,a.payload);
-      })
+      return action.payload;
+    });
+    builder.addCase(saveCurrent.fulfilled, (state, action) => {
+      const position = state.memes.findIndex((m) => m.id === action.payload.id);
+      if (position === -1) {
+        state.memes.push(action.payload);
+      } else {
+        state.memes[position] = action.payload;
+      }
+    });
+    /*demo ecoute daction d'autre tranches*/
+    builder.addCase(
+      updateCurrent,
+      (s, a: { type: string; payload: MemeInterface }) => {
+        console.log(
+          "depuis ressources j'ecoute laction de update current",
+          s,
+          a.payload,
+        );
+      },
+    );
   },
-
 });
 
-export const {} = ressourcesSlice.actions
+//export const {} = ressourcesSlice.actions
 
-const ressourcesReducer=ressourcesSlice.reducer
-export default ressourcesReducer
+const ressourcesReducer = ressourcesSlice.reducer;
+export default ressourcesReducer;
